5f5581f689e55667fef22262bf841edcac472d35
Faizan Patel <faizanpatel@Faizans-MacBook-Pro-14.local>
2023-07-14 10:19:55 +0100

    constructor added

--- RightAngleTriangle/src/main/java/example/test2/RightAngleTriangle.java
+++ RightAngleTriangle/src/main/java/example/test2/RightAngleTriangle.java
@@ -3,12 +3,16 @@ package example.test2;
 import example.test.Hypotenuse;
 
 public class RightAngleTriangle {
-    private double sideA, sideB, hypotenuse;
+    private double sideA, sideB;
+
+    public RightAngleTriangle(double sideA, double sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
 
     void setSideA(double a) {
         sideA = a;
     }
-
     void setSideB(double b) {
         sideB = b;
     }
@@ -16,7 +20,6 @@ public class RightAngleTriangle {
     double getSideA() {
         return sideA;
     }
-
     double getSideB() {
         return sideB;
     }
@@ -27,9 +30,7 @@ public class RightAngleTriangle {
 
     public static void main(String[] args)
     {
-        RightAngleTriangle triangle = new RightAngleTriangle();
-        triangle.setSideA(3.0);
-        triangle.setSideB(4.0);
+        RightAngleTriangle triangle = new RightAngleTriangle(3.0, 4.0);
         double ten = triangle.getHypotenuse();
         System.out.println("Hypotenuse is: " + ten);
     }
